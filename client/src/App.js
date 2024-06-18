e2dc2315bd5faad6ec6e3843ea30037f8540ab1f
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-06-18 19:53:07 -0400

    client/server split

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import {useState, useEffect} from 'react'
+import './App.css';
+
+function App() {
+  const [board, setBoard] = useState([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,])
+
+  return (
+    <div className="App">
+      hi
+    </div>
+  );
+}
+
+export default App;
