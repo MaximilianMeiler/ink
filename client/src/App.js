5ccbf91cde2ddd2f4ee4b506eb71f5d96e52e9bb
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-06-18 23:14:49 -0400

    room system

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,39 @@ import io from "socket.io-client";
 const socket = io.connect("http://localhost:4000");
 
 function App() {
-  const [board, setBoard] = useState([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1])
+  const [board, setBoard] = useState([]) //sync'ed with server
+  const [hand, setHand] = useState([])
+  const [room, setRoom] = useState(null)
 
   useEffect(() => {
     socket.on("newServerBoard", (newBoard) => {
-      setBoard(newBoard)
+      setBoard(newBoard);
     })
   }, [socket])
 
   function setSendBoard(newBoard) {
-    socket.emit("newClientBoard", newBoard);
+    socket.emit("newClientBoard", newBoard, room);
     setBoard(newBoard)
   }
 
   return (
     <div className="App">
+      <div style={{display: "flex"}}>
+        <p>Join room: </p>
+        <input type='text' id="roomInput"></input>
+        <button onClick={() => {
+          setRoom(document.getElementById('roomInput').value);
+          socket.emit("clientRoomJoin", document.getElementById('roomInput').value)
+        }}>Join</button>
+        <button onClick={() => {
+          setRoom(null);
+          setBoard([]);
+          if (room) {
+            socket.emit("clientRoomLeave", room)
+          }
+        }}>Leave room</button>
+      </div>
+      <div>Room: {room}</div>
       {board.map((val, index) => {
         return <div onClick={() => setSendBoard(board.map((v, i) => {return i === index ? 0 : v}))}>{val}</div>
       })}
--- server/index.js
+++ server/index.js
@@ -13,15 +13,47 @@ const io = new Server(server, {
   }
 })
 
-let serverBoard = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+let rooms = {};
 
 io.on("connection", (socket) => {
   console.log("New user: ", socket.id)
 
-  socket.emit("newServerBoard", serverBoard);
-  socket.on("newClientBoard", (newBoard) => {
-    serverBoard = newBoard;
-    socket.broadcast.emit("newServerBoard", newBoard);
+  socket.on("newClientBoard", (newBoard, room) => {
+    rooms[room].board = newBoard;
+    socket.to(room).emit("newServerBoard", newBoard);
+    console.log(rooms)
+  })
+
+  socket.on("clientRoomJoin", (room) => {
+    socket.join(room);
+    if (!rooms[room]) { //first player joins room (room created)
+      rooms[room] = {
+        board: [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+        players: [socket.id]
+      }
+    } else { //player joins pre-existing room
+      rooms[room].players = [...rooms[room].players, socket.id];
+    }
+    socket.emit("newServerBoard", rooms[room].board);
+  })
+
+  socket.on("clientRoomLeave", (room) => {
+    console.log(room)
+    if (rooms[room].players.length <= 1) { //last person leaves room
+      delete rooms[room];
+    } else {
+      rooms[room].players = rooms[room].players.filter((p) => p != socket.id);
+    }
+  })
+
+  socket.on("disconnect", () => {
+    //remove player from everywhere (same thing as clientRoomLeave but dumber)
+    Object.keys(rooms).forEach((room) => {
+      rooms[room].players = rooms[room].players.filter((p) => p != socket.id);
+      if (rooms[room].players.length < 1) {
+        delete rooms[room];
+      }
+    })
   })
 })
 
